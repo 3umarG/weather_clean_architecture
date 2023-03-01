2792b79d76d5f1468ebf7a69bfd6733d6e364a60
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-03-01 23:43:45 +0200

    Data Source in Data Layer

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,3 +1,4 @@
 class AppConstants {
   static const String BASE_URL = "https://api.openweathermap.org/data/2.5/";
+  static const String appKey = "a6f2a47ea7ac8390d5746920aa55e1e9";
 }
---
+++ lib/weather/data/data_source/remote_data_source.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:weather_clean_arch/core/utils/constants.dart';
+
+import '../models/weather_model.dart';
+import 'package:dio/dio.dart';
+
+abstract class BaseRemoteDataSource {
+  Future<WeatherModel?> getWeatherByCountryName(String countryName);
+}
+
+class RemoteDataSource extends BaseRemoteDataSource {
+  @override
+  Future<WeatherModel?> getWeatherByCountryName(String countryName) async {
+    try {
+      var response = await Dio().get(
+          "${AppConstants.BASE_URL}weather?q=$countryName&appid=${AppConstants.appKey}");
+      return WeatherModel.fromJson(json.decode(response.data));
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+}
