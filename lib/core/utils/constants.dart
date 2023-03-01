1b1aeded07f70169d442a940000101e5ea405144
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-03-01 17:00:19 +0200

    Init Commit Project Structure

---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,3 @@
+class AppConstants {
+  static const String BASE_URL = "http://api.weatherapi.com/v1/";
+}
---
+++ lib/weather/domain/entities/weather.dart
@@ -0,0 +1,15 @@
+class Weather {
+  final String name;
+  final String country;
+  final double lat;
+  final double lon;
+  final String tz_id;
+
+  Weather(
+    this.name,
+    this.country,
+    this.lat,
+    this.lon,
+    this.tz_id,
+  );
+}
